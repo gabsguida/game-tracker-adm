014b0f954fa711371bd54892942837b16fe69d7d
Gabriela Guida <gabrielaguida@gclaims.com.br>
2021-03-26 00:49:55 -0300

    add new offer - WIP

--- adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
+++ adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-ofertas',
@@ -18,17 +18,18 @@ export class CadastroOfertasComponent implements OnInit {
   ];
 
   ofertaId: number;
+  ofertas: any[] = [];
 
-  constructor(private fb: FormBuilder, private activatedRoute: ActivatedRoute) { }
+  constructor(private fb: FormBuilder, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.novaOfertaForm = this.fb.group({
-      id: [''],
-      titulo: [''],
-      preco: [''],
-      precoDesconto: [''],
+      id: ['', Validators.required],
+      titulo: ['', Validators.required],
+      preco: ['', Validators.required, Validators.min(1)],
+      precoDesconto: ['', Validators.required, Validators.min(1)],
       descricao: [''],
-      loja: ['']
+      loja: ['', Validators.required]
     });
 
     this.obterOfertaPorQueryParams();
@@ -37,10 +38,10 @@ export class CadastroOfertasComponent implements OnInit {
   obterOfertaPorQueryParams() {
     this.ofertaId = parseFloat(this.activatedRoute.snapshot.params.id);
 
-    let ofertas = JSON.parse(window.localStorage.getItem("ofertas-game-tracker"));
+    this.ofertas = JSON.parse(window.localStorage.getItem("ofertas-game-tracker"));
 
     if(this.ofertaId != 0 && this.ofertaId != undefined) {
-      let ofertaFiltrada = ofertas.filter(oferta => oferta.id === this.ofertaId)[0];
+      let ofertaFiltrada = this.ofertas.filter(oferta => oferta.id === this.ofertaId)[0];
       this.novaOfertaForm.setValue({
         id: ofertaFiltrada.id,
         titulo: ofertaFiltrada.titulo,
@@ -52,6 +53,54 @@ export class CadastroOfertasComponent implements OnInit {
     }
   }
 
-  
+  hasError(formGroup: FormGroup, formName: string, message?: string, invalidMessage?: string) {
+    let controls = formGroup.get(formName);
+    if (controls.errors != null) {
+      switch (true) {
+        case controls.errors['required'] && (controls.dirty || controls.touched): {
+          return message ? message : "Campo obrigatório"
+        }
+        case (controls.errors['email'] || controls.errors['mask']) && (controls.dirty || controls.touched): {
+          return invalidMessage ? invalidMessage : "Campo inválido"
+        }
+        case (controls.errors['depositAmountInvalid'] && (controls.dirty || controls.touched)):
+          return 'Valor do depósito maior do que o valor da fatura'
+        default:
+          break;
+      }
+    }
+  }
+
+  checkIdUnico() {
+    let id = this.novaOfertaForm.get('id').value;
+    let ofertasFiltrado = this.ofertas.filter(oferta => oferta.id === id);
+
+    console.log(ofertasFiltrado)
+
+    if(ofertasFiltrado.length > 0) {
+      this.novaOfertaForm.get('id').setErrors({ idInvalid: true });
+
+      console.log(this.novaOfertaForm)
+    } else {
+      this.novaOfertaForm.get('id').setErrors(null);
+    }
+  }
+
+  checkPrecoDescontoValido() {
+    let precoDesconto = this.novaOfertaForm.get('precoDesconto').value;
+    let preco = this.novaOfertaForm.get('preco').value;
+
+    if(precoDesconto > preco) {
+      this.novaOfertaForm.get('precoDesconto').setErrors({ precoDescontoInvalid: true })
+    } else {
+      this.novaOfertaForm.get('precoDesconto').setErrors(null)
+    }
+
+  }
+
+  salvarOferta() {
+    console.log(this.novaOfertaForm.value)
+    this.router.navigate(['nossasofertas']);
+  }  
 
 }
